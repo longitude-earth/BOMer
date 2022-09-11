c9df3af87d31e6216865904723309684abe58230
Matthias Adriaens <matthiasadriaens@gmail.com>
2022-09-11 17:04:54 +0200

    fix(env): fix required env variables

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,14 +8,6 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
-  GITHUB_ID: z.string(),
-  GITHUB_SECRET: z.string(),
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string(),
-  EMAIL_SERVER: z.string(),
-  EMAIL_FROM: z.string()
 });
 
 /**
